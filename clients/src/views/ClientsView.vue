5d6a04bce9005ecf14278354f2f580bd5d4c760b
MarieVerane <verane.n-gouan@epitech.eu>
2026-02-18 12:40:39 +0000

    aa

--- clients/src/views/ClientsView.vue
+++ clients/src/views/ClientsView.vue
@@ -68,7 +68,7 @@
             :disabled="currentPage === 1"
             @click="prevPage"
           >
-            Precedent
+            Precedenteeee
           </button>
           <button
             class="px-3 py-1 border rounded disabled:opacity-50"
